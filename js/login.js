eab0bb964c4b4506647ee64634c694678b6fed8b
CiMiKo <private.hartono@mail.com>
2021-01-09 00:39:35 +0700

    Menambahkan Form Register

---
+++ js/login.js
@@ -0,0 +1,10 @@
+let userKasir = [];
+
+const showHide = () => {
+    document.querySelector('.login').classList.toggle('hidden');
+    document.querySelector('.register').classList.toggle('hidden');
+}
+
+const register = () => {
+    let form = document.registerForm
+}
